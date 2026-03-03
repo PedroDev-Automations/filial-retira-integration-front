f44151995e96ad8afa76369a443f8bbe854c3aaa
Pedro <peheguibra@gmail.com>
2026-03-03 16:22:34 -0300

    feat(api): cria servico HTTP para comunicacao com o backend em Spring Boot

---
+++ filial-retira-front/src/app/core/services/integracao.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IntegracaoService {
+  // A URL oficial da sua VPS!
+  private apiUrl = 'http://62.171.182.180:8080/api/integracao';
+
+  constructor(private http: HttpClient) {}
+
+  // Busca todas as configurações salvas no banco
+  listarConfiguracoes(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/configuracao`);
+  }
+
+  // Salva ou atualiza uma Matriz ou Filial
+  salvarConfiguracao(config: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/configuracao`, config, { responseType: 'text' });
+  }
+
+  // O "Botão de Pânico" para forçar a busca de pedidos manualmente
+  forcarVarreduraPedidos(): Observable<any> {
+    return this.http.post(`${this.apiUrl}/pedidos/varredura`, {}, { responseType: 'text' });
+  }
+
+  // Força a carga completa de estoque
+  forcarCargaEstoque(): Observable<any> {
+    return this.http.post(`${this.apiUrl}/carga-completa`, {}, { responseType: 'text' });
+  }
+}
