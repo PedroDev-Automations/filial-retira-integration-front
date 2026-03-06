0558e4c4abeaffb0c95f2f715a992274e1c388d3
Pedro <peheguibra@gmail.com>
2026-03-06 16:25:31 -0300

    chore: remove IP fixo de producao e transforma environment em template

--- filial-retira-front/src/environments/environment.development.ts
+++ filial-retira-front/src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  // Aqui você coloca a URL do seu Spring Boot rodando no IntelliJ/Eclipse na sua máquina
+  // Sempre roda no seu servidor local (IntelliJ) durante o desenvolvimento
   apiUrl: 'http://localhost:8080/api/integracao' 
 };
-
--- filial-retira-front/src/environments/environment.ts
+++ filial-retira-front/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // Aqui é a URL definitiva da sua VPS!
-  apiUrl: 'http://62.171.182.180:8080/api/integracao'
+  // Esse valor é substituído automaticamente na nuvem pelo set-env.js
+  apiUrl: '/api/integracao' 
 };
