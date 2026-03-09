9af26369089d1b208b02e9d4d1b9966445c78dfb
Pedro <peheguibra@gmail.com>
2026-03-09 18:17:32 -0300

    feat: integra rotas protegidas, interceptor global e isola layout principal

--- filial-retira-front/src/app/app.routes.ts
+++ filial-retira-front/src/app/app.routes.ts
@@ -2,10 +2,18 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard';
 import { MatrizConfigComponent } from './pages/matriz-config/matriz-config';
 import { FiliaisConfigComponent } from './pages/filiais-config/filiais-config';
+import { LoginComponent } from './pages/login/login';
+import { SegurancaConfigComponent } from './pages/seguranca-config/seguranca-config'; 
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'matriz', component: MatrizConfigComponent },
-  { path: 'filiais', component: FiliaisConfigComponent }
+  
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: 'matriz', component: MatrizConfigComponent, canActivate: [authGuard] },
+  { path: 'filiais', component: FiliaisConfigComponent, canActivate: [authGuard] },
+  { path: 'seguranca', component: SegurancaConfigComponent, canActivate: [authGuard] },
+
+  { path: '**', redirectTo: 'login' } 
 ];
--- filial-retira-front/src/app/app.ts
+++ filial-retira-front/src/app/app.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 import { SidebarComponent } from './layout/sidebar/sidebar'; 
 import { NavbarComponent } from './layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, NavbarComponent], 
+  imports: [CommonModule, RouterOutlet, SidebarComponent, NavbarComponent],
   templateUrl: './app.html', 
   styleUrl: './app.css'     
 })
 export class App {
   title = 'filial-retira-front';
+
+  constructor(public router: Router) {} 
 }
